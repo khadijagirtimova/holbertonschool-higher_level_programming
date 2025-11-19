bacadc8bb31018771789c70499b35a15c90d8e00
khadijagirtimova <xadica.qirtimova@gmail.com>
2025-11-19 14:25:19 +0000

    task

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ == "__main__":
-total = 0
-i = 1
-while i < len(sys.argv):
-total += int(sys.argv[i])
-i += 1
-print("{}".format(total))
+    total = 0
+    i = 1
+    while i < len(sys.argv):
+        total += int(sys.argv[i])
+        i += 1
+    print("{}".format(total))

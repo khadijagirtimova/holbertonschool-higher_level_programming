28caa97ce2b644fe5bbeb1099c5a4434a6cba823
khadijagirtimova <xadica.qirtimova@gmail.com>
2025-11-26 14:16:58 +0000

    task

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -34,9 +34,14 @@ class Rectangle:
         self.__height = value
 def area(self):
     """Returns the rectangle area."""
+
+
     return self.width * self.height
 def perimeter(self):
     """Returns the rectangle perimeter."""
+
+    
     if self.width == 0 or self.height == 0:
         return 0
     return 2 * (self.width + self.height)
+

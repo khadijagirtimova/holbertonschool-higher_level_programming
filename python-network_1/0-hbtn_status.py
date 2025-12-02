e71b206e2e8fb91820b60d96e9a684d5e735ae82
khadijagirtimova <xadica.qirtimova@gmail.com>
2025-12-02 14:48:43 +0000

    task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Fetches https://intranet.hbtn.io/status using urllib.
+"""
+
 import urllib.request
 
 with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:

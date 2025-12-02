37609a9795c872dcc2bab29e9e0ad81abfd2d565
khadijagirtimova <xadica.qirtimova@gmail.com>
2025-12-02 15:49:01 +0000

    task

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
-Python script that fetches a URL and displays
+"""Sends a POST request with an email parameter and displays the response."""
 
-"""
-import sys
 import urllib.request
 import urllib.parse
+import sys
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
+    # Encode the POST data
     data = urllib.parse.urlencode({"email": email}).encode("utf-8")
 
-
-    with urllib.request.urlopen.(url, data) as response:
+    # Send POST request
+    with urllib.request.urlopen(url, data) as response:
         body = response.read()
 
-        print(body.decode("utf-8"))
+    # Decode and print response body
+    print(body.decode("utf-8"))
